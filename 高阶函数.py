b2fc8dd43f9e5ec8a0ebbfb7f366731dd56a364c
Si-Cong Peng <psc02031314@gmail.com>
2017-11-24 21:01:45 +0800

    add 高阶函数

---
+++ 高阶函数.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python  
+# encoding: utf-8  
+
+""" 
+
+File Name: 高阶函数.py
+
+Created by 彭思聪 on 2017/11/24 下午8:52.
+Copyright © 2017年 彭思聪. All rights reserved.
+
+"""  
+
+
+def func(x):
+    return x * x
+
+
+# 高阶函数，f做为参数名传入函数
+def foo(a, b, f):
+    return f(a) + f(b)
+
+
+if __name__ == '__main__':
+    result = foo(2, 3, func)
+    print(result)
