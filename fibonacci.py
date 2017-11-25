2286fce24666c76c7ac880547f6c7c1c02c0148a
Si-Cong Peng <psc02031314@gmail.com>
2017-11-25 17:57:24 +0800

    add 生成器与迭代器

--- fibonacci.py
+++ fibonacci.py
@@ -8,14 +8,85 @@ File Name: fibonacci.py
 Created by 彭思聪 on 2017/11/24 下午9:21.
 Copyright © 2017年 彭思聪. All rights reserved.
 
-"""  
+"""
+
+import time
+from functools import wraps
 
 
-def fibonacci(n):
-    if n == 1 or n == 0:
+def speed_test(func):
+
+    @wraps(func)
+    def wrapper(*args):
+        start = time.time()
+        func(*args)
+        end = time.time()
+        duration = end - start
+        duration *= 1000
+        print('%s cost %0.2f ms' % (wrapper.__name__, duration))
+    return wrapper
+
+
+# 普通版本，
+def fibonacci(maxnum):
+    a = 0
+    b = 1
+    n = 0
+    fibo = []
+    while n < maxnum:
+        fibo.append(a)
+        a, b = b, a+b
+        n += 1
+    return fibo
+
+
+# 递归版本，给定一个n输出fibonacci数列的第n个值
+def fibonacci_recursion(n):
+    if n == 0:
+        return 0
+    if n == 1:
         return 1
-    return fibonacci(n-2) + fibonacci(n-1)
+    return fibonacci_recursion(n-2) + fibonacci_recursion(n-1)
 
 
-for i in range(10):
-    print(i, ' : ', fibonacci(i))
+# 生成器版本
+def fibonacci_generator(maxnum):
+    a = 0
+    b = 1
+    n = 0
+    while n < maxnum:
+        yield a
+        a, b = b, a + b
+        n += 1
+
+
+@speed_test
+def normal_edit(num):
+    print('普通版本：', end='')
+    fibo_list = fibonacci(num)
+    for item in fibo_list:
+        print(item, end=' ')
+    print()
+
+
+@speed_test
+def recursion_edit(num):
+    print('递归版本：', end='')
+    for i in range(num):
+        print(fibonacci_recursion(i), end=' ')
+    print()
+
+
+@speed_test
+def generator_edit(num):
+    print('生成器版本：', end='')
+    for item in fibonacci_generator(num):
+        print(item, end=' ')
+    print()
+
+
+if __name__ == '__main__':
+    n = 100
+    normal_edit(n)
+    # recursion_edit(n)
+    generator_edit(n)
---
+++ 生成器与迭代器.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python  
+# encoding: utf-8  
+
+""" 
+
+File Name: 生成器与迭代器.py
+
+Created by 彭思聪 on 2017/11/25 下午1:11.
+Copyright © 2017年 彭思聪. All rights reserved.
+
+"""
+
+# from collections import Generator, Iterator, Iterable
+
+"""
+生成器(Generator)
+    生成器是一个可迭代对象（Iterable）
+    
+    创建方式:
+        1. (f(x) for x in range(10))
+        2. yield 封装了iter和next
+"""
+
+
+"""
+迭代器(Iterator)
+    满足两个条件
+        1. 有iter方法
+        2. 有next方法
+
+可迭代对象(Iterable)
+    满足条件：
+        有iter方法
+    
+    
+"""
+
+
+"""
+list_ = [1, 2, 3]        # 可迭代对象，有__iter__方法，可以被for迭代，但是没有__next__方法
+list_iter = iter(list_)  # 内置函数将list_转换为一个迭代器，拥有了__next__方法
+
+iter(L)
+Out[4]: <list_iterator at 0x10a658828>
+
+list_iter = iter(L)
+
+list_iter.__next__()
+Out[6]: 1
+
+list_iter.__next__()
+Out[7]: 2
+
+list_iter.__next__()
+Out[8]: 3
+
+list_iter.__next__()
+Traceback (most recent call last):
+  File "/usr/local/lib/python3.5/site-packages/IPython/core/interactiveshell.py", line 2881, in run_code
+    exec(code_obj, self.user_global_ns, self.user_ns)
+  File "<ipython-input-9-4a6c6e6ddc82>", line 1, in <module>
+    list_iter.__next__()
+StopIteration
+"""
+
+"""
+for 循环内部的三件事
+    1. 调用可迭代对象的iter方法返回一个迭代器对象
+    2. 不断调用迭代器对象的next方法
+    3. 处理StopIteration异常
+"""
+
+
+"""
+def f(x):
+    return 3 * x
+
+
+# map 类型的生成器，利用__next__调用，相当于获取菜单的厨师并通过__next__一盘一盘按顺序上菜，占用内存少
+print(map(f, range(10)))    # <map object at 0x106c50ba8>
+
+# 生成器
+print((f(x) for x in range(10)))   # <generator object <genexpr> at 0x1087b0af0>
+
+# 列表生成器， 直接将所有内容列出， 相对一桌子菜，占用内存多, 但是比生成器灵活
+print([f(x) for x in range(10)])    # [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+"""
+
+
+"""
+def func(x):
+    for i in range(x):
+        yield i * i
+    print('end')
+    return
+
+
+gen = func(10)
+
+print(gen)  # <generator object func at 0x10107caf0>
+print('first:', next(gen))
+print('second:', next(gen))
+print('third:', next(gen))
+
+
+# for循环迭代可迭代对象, 都有可迭代方法__iter__
+for item in gen:
+    print('others:', item)
+"""
+
+
+"""
+def foo():
+    count1 = yield 1
+    print(count1)
+
+    count2 = yield 2
+    print(count2)
+
+    yield 3
+
+
+g = foo()
+print(g)
+print(g.send(None))  # g.send(None) 等价与 next(g)
+
+print(g.send('hello'))  # 将'hello'传到count1，然后调用__next__来执行yield 2
+print(g.send('world'))  # 将'world'传到count2，然后调用__next__来执行yield 3
+"""
